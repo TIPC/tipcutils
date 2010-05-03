24e371b1d22947a4e9a5930b0f962cf7ad10f4af
Allan Stephens <allan.stephens@windriver.com>
2010-05-03 11:26:24 -0400

    Update connection demo for TIPC 2.0
    Make minor changes to update the connection demo for TIPC 2.0, including:
    
    - adding support for endianness in topology server messages
    - standardizing comments and formatting to match other TIPC applications
    
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>

--- demos/connection_demo/client_tipc.c
+++ demos/connection_demo/client_tipc.c
@@ -1,175 +1,167 @@
 /* ------------------------------------------------------------------------
-//
-// client_tipc.c
-//
-// Short description: This progrem sends a hello world message, then
-// waits for an acknowledge before exiting.
-// ------------------------------------------------------------------------
-//
-// Copyright (c) 2003, Ericsson Research Canada
-// All rights reserved.
-// Redistribution and use in source and binary forms, with or without 
-// modification, are permitted provided that the following conditions are met:
-//
-// Redistributions of source code must retain the above copyright notice, this 
-// list of conditions and the following disclaimer.
-// Redistributions in binary form must reproduce the above copyright notice, 
-// this list of conditions and the following disclaimer in the documentation 
-// and/or other materials provided with the distribution.
-// Neither the name of Ericsson Research Canada nor the names of its 
-// contributors may be used to endorse or promote products derived from this 
-// software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-// POSSIBILITY OF SUCH DAMAGE.
-//
-// ------------------------------------------------------------------------
-//
-//  Created 2000-09-30 by Jon Maloy
-//
-// ------------------------------------------------------------------------
-//
-//  $Id: client_tipc.c,v 1.6 2005/10/24 21:18:34 jonmaloy Exp $
-//
-//  Revision history:
-//  ----------------
-//  Rev	Date		Rev by	Reason
-//  ---	----		------	------
-//
-//  PA1	2000-09-30	Jon Maloy	Created
-//
-// ------------------------------------------------------------------------
-*/
+ *
+ * client_tipc.c
+ *
+ * Short description: TIPC connection demo (client side)
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Copyright (c) 2003, Ericsson Research Canada
+ * Copyright (c) 2010 Wind River Systems
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution.
+ * Neither the name of Ericsson Research Canada nor the names of its 
+ * contributors may be used to endorse or promote products derived from this 
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ * ------------------------------------------------------------------------
+ */
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <assert.h>
 #include <string.h>
 #include <sys/param.h>
-#include <sys/poll.h>
-#include <sys/ioctl.h>
-#include <netdb.h>
 #include <errno.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <sys/time.h>
-#include <ctype.h>
+#include <netinet/in.h>
 #include <linux/tipc.h>
 
 #define SERVER_TYPE  18888
 #define SERVER_INST  17
 
-void wait_for_server(struct tipc_name* name,int wait)
+void wait_for_server(__u32 name_type, __u32 name_instance, int wait)
 {
-        struct sockaddr_tipc topsrv;
-        struct tipc_subscr subscr = {{name->type,name->instance,name->instance},
-                                     wait,TIPC_SUB_SERVICE,{}};
-        struct tipc_event event;
+	struct sockaddr_tipc topsrv;
+	struct tipc_subscr subscr;
+	struct tipc_event event;
 
-        int sd = socket (AF_TIPC, SOCK_SEQPACKET,0);
-        assert(sd > 0);
+	int sd = socket(AF_TIPC, SOCK_SEQPACKET, 0);
 
-        memset(&topsrv,0,sizeof(topsrv));
+	memset(&topsrv, 0, sizeof(topsrv));
 	topsrv.family = AF_TIPC;
-        topsrv.addrtype = TIPC_ADDR_NAME;
-        topsrv.addr.name.name.type = TIPC_TOP_SRV;
-        topsrv.addr.name.name.instance = TIPC_TOP_SRV;
+	topsrv.addrtype = TIPC_ADDR_NAME;
+	topsrv.addr.name.name.type = TIPC_TOP_SRV;
+	topsrv.addr.name.name.instance = TIPC_TOP_SRV;
 
-        /* Connect to topology server: */
+	/* Connect to topology server */
 
-        if (0 > connect(sd,(struct sockaddr*)&topsrv,sizeof(topsrv))){
-                perror("Failed to connect to topology server");
-                exit(1);
-        }
-        if (send(sd,&subscr,sizeof(subscr),0) != sizeof(subscr)){
-                perror("Failed to send subscription");
-                exit(1);
-        }
-        /* Now wait for the subscription to fire: */
-        if (recv(sd,&event,sizeof(event),0) != sizeof(event)){
-                perror("Failed to receive event");
-                exit(1);
-        }
-        if (event.event != TIPC_PUBLISHED){
-                printf("Server %u,%u not published within %u [s]\n",
-                       name->type,name->instance,wait/1000);
-                exit(1);
-        }
-        close(sd);
+	if (0 > connect(sd, (struct sockaddr *)&topsrv, sizeof(topsrv))) {
+		perror("Client: failed to connect to topology server");
+		exit(1);
+	}
+
+	subscr.seq.type = htonl(name_type);
+	subscr.seq.lower = htonl(name_instance);
+	subscr.seq.upper = htonl(name_instance);
+	subscr.timeout = htonl(wait);
+	subscr.filter = htonl(TIPC_SUB_SERVICE);
+
+	if (send(sd, &subscr, sizeof(subscr), 0) != sizeof(subscr)) {
+		perror("Client: failed to send subscription");
+		exit(1);
+	}
+	/* Now wait for the subscription to fire */
+
+	if (recv(sd, &event, sizeof(event), 0) != sizeof(event)) {
+		perror("Client: failed to receive event");
+		exit(1);
+	}
+	if (event.event != htonl(TIPC_PUBLISHED)) {
+		printf("Client: server {%u,%u} not published within %u [s]\n",
+		       name_type, name_instance, wait/1000);
+		exit(1);
+	}
+
+	close(sd);
 }
 
 
-int main(int argc, char* argv[], char* dummy[])
+int main(int argc, char *argv[], char *dummy[])
 {
-        int sd;
-        struct sockaddr_tipc server_addr;
-        char buf[40];
+	int sd;
+	struct sockaddr_tipc server_addr;
+	char buf[40];
+
+	printf("****** TIPC connection demo client started ******\n\n");
+
+	wait_for_server(SERVER_TYPE, SERVER_INST, 10000);
 
 	server_addr.family = AF_TIPC;
-        server_addr.addrtype = TIPC_ADDR_NAME;
-        server_addr.addr.name.name.type = SERVER_TYPE;
-        server_addr.addr.name.name.instance = SERVER_INST;
-        server_addr.addr.name.domain = 0;
+	server_addr.addrtype = TIPC_ADDR_NAME;
+	server_addr.addr.name.name.type = SERVER_TYPE;
+	server_addr.addr.name.name.instance = SERVER_INST;
+	server_addr.addr.name.domain = 0;
 
-        printf("****** TIPC connection demo client started ******\n\n");
-
-        wait_for_server(&server_addr.addr.name.name,10000);
-  
-        printf("Connection setup 1: standard (TCP style) connect\n");
-        sd = socket (AF_TIPC, SOCK_SEQPACKET, 0);
-        if (connect(sd, (struct sockaddr *)&server_addr,
-                sizeof(server_addr)) != 0) {
-                perror("Connect failed");
-	}
-        printf("Client: Connection established\n");
-        strcpy(buf, "Hello World");
-        if (0 > send(sd,buf,strlen(buf)+1,0)){
-                perror("Client: Failed to send");
-        }
-        printf("Client: Sent msg \"%s\" \n", buf);
-        if (0 >= recv(sd, buf, sizeof(buf), 0)) {
-                perror("Client: Failed to receive");
-        }
-        printf("Client: Received response \"%s\" \n", buf);
-        printf("Client: Shutting down connection\n\n");
-        shutdown(sd, SHUT_RDWR);
-        close(sd);
-
-        printf("Connection setup 2: optimized (TIPC style) connect\n");
+	printf("Client: connection setup 1 - standard (TCP style) connect\n");
 	sd = socket (AF_TIPC, SOCK_SEQPACKET, 0);
-        strcpy(buf, "Hello Again");
-        if (0 > sendto(sd, buf, strlen(buf)+1, 0,
-                       (struct sockaddr *)&server_addr,
-                       sizeof(server_addr))) {
-                perror("Client: Failed to send");
-        }
-        printf("Client: Sent msg \"%s\" \n", buf);
-        if (0 >= recv(sd, buf, sizeof(buf), 0)) {
-                perror("Client: Failed to receive");
-        }
-        printf("Client: Received response \"%s\"\n", buf);
-        printf("Client: Killing connection without shutdown\n\n");
-        close(sd);
+	if (connect(sd, (struct sockaddr *)&server_addr,
+		    sizeof(server_addr)) != 0) {
+		perror("Client: connect failed");
+		exit(1);
+	}
+	printf("Client: connection established\n");
+	strcpy(buf, "Hello World");
+	if (0 > send(sd, buf, strlen(buf)+1, 0)) {
+		perror("Client: failed to send");
+		exit(1);
+	}
+	printf("Client: Sent msg \"%s\" \n", buf);
+	if (0 >= recv(sd, buf, sizeof(buf), 0)) {
+		perror("Client: failed to receive");
+		exit(1);
+	}
+	printf("Client: received response \"%s\" \n", buf);
+	printf("Client: shutting down connection\n\n");
+	shutdown(sd, SHUT_RDWR);
+	close(sd);
 
-        printf("Connection setup 3: optimized (TIPC style) connect\n");
-        sd = socket (AF_TIPC, SOCK_SEQPACKET,0);
-        strcpy(buf, "Hello Again Again");
-        if (0 > sendto(sd, buf, strlen(buf)+1, 0,
-                       (struct sockaddr*)&server_addr,
-                       sizeof(server_addr))) {
-                perror("Client: Failed to send");
-        }
-        printf("Client: Sent msg \"%s\" \n", buf);
-        printf("Client: Will now exit without closing socket!!\n\n");
-        exit(0);
+	printf("Client: connection setup 2 - optimized (TIPC style) connect\n");
+	sd = socket(AF_TIPC, SOCK_SEQPACKET, 0);
+	strcpy(buf, "Hello Again");
+	if (0 > sendto(sd, buf, strlen(buf)+1, 0,
+		       (struct sockaddr *)&server_addr, sizeof(server_addr))) {
+		perror("Client: failed to send");
+		exit(1);
+	}
+	printf("Client: sent msg \"%s\" \n", buf);
+	if (0 >= recv(sd, buf, sizeof(buf), 0)) {
+		perror("Client: failed to receive");
+		exit(1);
+	}
+	printf("Client: received response \"%s\"\n", buf);
+	printf("Client: killing connection without shutdown\n\n");
+	close(sd);
+
+	printf("Client: connection setup 3 - optimized (TIPC style) connect\n");
+	sd = socket (AF_TIPC, SOCK_SEQPACKET,0);
+	strcpy(buf, "Hello Again Again");
+	if (0 > sendto(sd, buf, strlen(buf)+1, 0,
+		       (struct sockaddr*)&server_addr, sizeof(server_addr))) {
+		perror("Client: failed to send");
+		exit(1);
+	}
+	printf("Client: sent msg \"%s\"\n", buf);
+	printf("Client: will now exit without closing socket!!\n\n");
+	exit(0);
 }
--- demos/connection_demo/server_tipc.c
+++ demos/connection_demo/server_tipc.c
@@ -1,55 +1,40 @@
 /* ------------------------------------------------------------------------
-//
-// server_tipc.c
-//
-// Short description: This progrem waits for a hello message to be
-// recieved, sends back an acknowledge and then exits.
-// ------------------------------------------------------------------------
-//
-// Copyright (c) 2003, Ericsson Research Canada
-// All rights reserved.
-// Redistribution and use in source and binary forms, with or without 
-// modification, are permitted provided that the following conditions are met:
-//
-// Redistributions of source code must retain the above copyright notice, this 
-// list of conditions and the following disclaimer.
-// Redistributions in binary form must reproduce the above copyright notice, 
-// this list of conditions and the following disclaimer in the documentation 
-// and/or other materials provided with the distribution.
-// Neither the name of Ericsson Research Canada nor the names of its 
-// contributors may be used to endorse or promote products derived from this 
-// software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-// POSSIBILITY OF SUCH DAMAGE.
-//
-// ------------------------------------------------------------------------
-//
-//  Created 2000-09-30 by Jon Maloy
-//
-// ------------------------------------------------------------------------
-//
-//  $Id: server_tipc.c,v 1.4 2006/11/30 14:58:29 ajstephens Exp $
-//
-//  Revision history:
-//  ----------------
-//  Rev	Date		Rev by	Reason
-//  ---	----		------	------
-//
-//  PA1	2001-03-11	Jon Maloy	Created
-//  PA2 2004-03-25      M. Pourzandi    Simplified to support a simple hello message
-//
-// ------------------------------------------------------------------------
-*/
+ *
+ * server_tipc.c
+ *
+ * Short description: TIPC connection demo (server side)
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Copyright (c) 2003, Ericsson Research Canada
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution.
+ * Neither the name of Ericsson Research Canada nor the names of its 
+ * contributors may be used to endorse or promote products derived from this 
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ------------------------------------------------------------------------
+ */
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdlib.h>
@@ -57,11 +42,7 @@
 #include <assert.h>
 #include <string.h>
 #include <sys/param.h>
-#include <sys/poll.h>
-#include <sys/ioctl.h>
-#include <netdb.h>
 #include <errno.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <linux/tipc.h>
 
@@ -69,70 +50,73 @@
 #define SERVER_INST  17
 #define BUF_SZ 40
 
-int main(int argc, char* argv[], char* dummy[])
+int main(int argc, char *argv[], char *dummy[])
 {
-  int listener_sd = socket (AF_TIPC, SOCK_SEQPACKET,0);
-  struct sockaddr_tipc server_addr;
-  int conn_count;
+	struct sockaddr_tipc server_addr;
+	int listener_sd;
+	int conn_count;
 
-  server_addr.family = AF_TIPC;
-  server_addr.addrtype = TIPC_ADDR_NAMESEQ;
-  server_addr.addr.nameseq.type = SERVER_TYPE;
-  server_addr.addr.nameseq.lower = SERVER_INST;
-  server_addr.addr.nameseq.upper = SERVER_INST;
-  server_addr.scope = TIPC_ZONE_SCOPE;
+	server_addr.family = AF_TIPC;
+	server_addr.addrtype = TIPC_ADDR_NAMESEQ;
+	server_addr.addr.nameseq.type = SERVER_TYPE;
+	server_addr.addr.nameseq.lower = SERVER_INST;
+	server_addr.addr.nameseq.upper = SERVER_INST;
+	server_addr.scope = TIPC_ZONE_SCOPE;
 
-  printf("****** TIPC connection demo server started ******\n");
+	printf("****** TIPC connection demo server started ******\n");
 
-  /* Make server available: */
+	listener_sd = socket (AF_TIPC, SOCK_SEQPACKET, 0);
 
-  if (0 != bind (listener_sd, (struct sockaddr*)&server_addr,sizeof(server_addr))){
-          printf ("Server: Failed to bind port name\n");
-          exit (1);
-  }
+	if (0 != bind(listener_sd, (struct sockaddr *)&server_addr,
+		      sizeof(server_addr))) {
+		printf("Server: failed to bind port name\n");
+		exit(1);
+	}
 
-  if (0 != listen (listener_sd, 0)) {
-          printf ("Server: Failed to listen\n");
-          exit (1);
-  }
-  
-  for (conn_count = 1; conn_count <= 3; conn_count++) {
-	  int peer_sd;
-	  int sz;
-	  char inbuf[BUF_SZ];
-	  char outbuf[BUF_SZ];
+	if (0 != listen(listener_sd, 0)) {
+		printf ("Server: failed to listen\n");
+		exit(1);
+	}
 
-          peer_sd = accept(listener_sd, 0, 0);
-          if (peer_sd < 0 ){
-                  printf ("Server: accept failed\n");
-                  exit(1);
-          }
-          printf("\nServer: accept() returned\n");
-          if (!fork()) {
-                  printf ("Server process %d created \n", conn_count);    
-                  while (1) {
-                          sz = recv(peer_sd, inbuf, BUF_SZ, 0);
-			  if (sz == 0) {
-				  printf ("Server %d: client terminated normally\n", 
-					  conn_count);    
-				  exit(0);
-			  }
-			  if (sz < 0) {
-				  printf ("Server %d : client terminated abnormally\n",
-					  conn_count);    
-				  exit(1);
-			  }
-                          printf ("Server %d: received msg \"%s\"\n",
-				  conn_count, inbuf);    
-			  sprintf(outbuf, "Response for test %d", conn_count);
-                          if (0 >= send(peer_sd, outbuf, strlen(outbuf)+1, 0)) {
-                                  printf("Server %d : Failed to send response\n",
-					 conn_count);
-                          }
-                          printf ("Server %d: responded with \"%s\"\n",
-				  conn_count, outbuf);    
-                  }
-          }
-  }
-  exit(0);
+	for (conn_count = 1; conn_count <= 3; conn_count++) {
+		int peer_sd;
+		int sz;
+		char inbuf[BUF_SZ];
+		char outbuf[BUF_SZ];
+
+		peer_sd = accept(listener_sd, 0, 0);
+		if (peer_sd < 0 ) {
+			printf ("Server: accept failed\n");
+			exit(1);
+		}
+		printf("\nServer: accept() returned\n");
+		if (!fork()) {
+			printf ("Server process %d created \n", conn_count);    
+			while (1) {
+				sz = recv(peer_sd, inbuf, BUF_SZ, 0);
+				if (sz == 0) {
+					printf("Server %d: client terminated normally\n", 
+					       conn_count);    
+					exit(0);
+				}
+				if (sz < 0) {
+					printf("Server %d : client terminated abnormally\n",
+					       conn_count);    
+					exit(1);
+				}
+				printf("Server %d: received msg \"%s\"\n",
+				       conn_count, inbuf);    
+				sprintf(outbuf, "Response for test %d", conn_count);
+				if (0 >= send(peer_sd, outbuf, strlen(outbuf)+1, 0)) {
+					printf("Server %d : failed to send response\n",
+					       conn_count);
+					exit(1);
+				}
+				printf("Server %d: responded with \"%s\"\n",
+				       conn_count, outbuf);    
+			}
+		}
+	}
+
+	exit(0);
 }
